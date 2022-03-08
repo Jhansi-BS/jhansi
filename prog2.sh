1465d57f11872994a46eaf5fc5296de6c366ad12
rashmi <rashmidivakar55@gmail.com>
2022-03-08 15:16:08 +0530

    haii

--- prog2.sh
+++ prog2.sh
@@ -1,23 +1,28 @@
-file1=$1
-file2=$2
+echo "enter the first file name:"
+read file1
 if [ -e $file ]
 then
-set-- `ls -ld $file1`
+set -- `ls -ld $file1`
 file1perm=$1
 else
 echo " file doesnot exit "
 exit
 fi
+echo "enter second file name:"
+read file2
 if [ -e $file2 ]
 then
-set-- `ls -ld $file2`
-file2perm=$2
+set -- `ls -ld $file2`
+file2perm=$1
 else
 echo " file does not exit "
 exit
 fi
 if [ $file1perm = $file2perm ]
 then
+echo "file permission are identical"
+echo " permission is $file1perm"
+else
 echo " file permission are not identical "
 echo "$file1 permission is $file1perm "
 echo "$file2 permission is $file2perm"
