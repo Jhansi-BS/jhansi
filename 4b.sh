6c410445b52f896c8672e3414ff7eb7460308e61
Jhansi-BS <100575806+Jhansi-BS@users.noreply.github.com>
2022-05-20 15:27:33 +0530

    Update 4b.sh

--- 4b.sh
+++ 4b.sh
@@ -1,17 +1,20 @@
-if [ $# -eq 3 ]
+if [ $# -ge 1 ]
 then
-if [ -f $1 ]
-then
-l =` cat $1 | wc -l`
-if [ $2 -gt $3 -a $2 -gt $1 a $3 -gt $2 ]
-then
-head -n $1 $3 | tail -n +$2
+   if [ $# -eq 2 ]
+   then
+	chd=$2
+   else
+	chd= `cd .`
+   fi
+   file= `find $chd -iname $1`
+   if [ `echo $file | wc -c` -gt 1 ]
+   then
+     number =`ls -inum $file | grep -o ^[0-9]*`
+     cd $chd
+     find $chd -inum $number
+   else
+     echo "No such file"
+  fi
 else
-echo "enter valid argument"
-fi
-else
-echo "file doesnt exist"
-fi
-else
-echo "entre the nunber of argument"
+echo "Enter argument"
 fi
